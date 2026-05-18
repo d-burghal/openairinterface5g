b740545a5cfce7dd02145d3a3a56c3aa069b0275
Teodora Vladić <teodora.vladic@openairinterface.org>
2026-05-18 10:58:22 +0200

    Delete unnecessary fronthaul PRACH FIFO queue
    Previously, we stored the PRACH information for each TTI through a
    queue. Later through `oran_fh_if4p5_south_in()` we called
    `xran_fh_rx_prach_read_slot()` and then checked if PRACH slot, process
    IQ samples.
    
    This commit avoids calling too many functions. Instead, it resets the
    nRxPkt of each TTI to 0. If the current TTI is the PRACH slot, then
    extract PRACH IQ samples.
    
    => PRACH is processed through the callback only.
    
    Signed-off-by: Teodora Vladić <teodora.vladic@openairinterface.org>

--- radio/fhi_72/oran_isolate.c
+++ radio/fhi_72/oran_isolate.c
@@ -34,7 +34,6 @@ typedef struct {
 } oran_eth_state_t;
 
 notifiedFIFO_t oran_sync_fifo;
-notifiedFIFO_t oran_sync_fifo_prach;
 
 int trx_oran_start(openair0_device_t *device)
 {
@@ -132,33 +131,23 @@ int trx_oran_get_stats(openair0_device_t *device)
 
 void oran_fh_if4p5_south_in(RU_t *ru, int *frame, int *slot)
 {
-  int ret = 0; // return code for PUSCH/PRACH processing
-
   ru_info_t ru_info = {
       .nb_rx = ru->nb_rx,
       .nb_tx = ru->nb_tx,
       .rxdataF = ru->common.rxdataF,
       .beam_id = ru->common.beam_id,
-      .prach_buf = NULL,
   };
 
-  /* Firstly, process PUSCH packets */
+  /* Process PUSCH packets */
   RU_proc_t *proc = &ru->proc; // to check if (frame,slot) combination corresponds to the expected PUSCH one
   int f, sl;
   LOG_D(HW, "Read rxdataF %p,%p\n", ru_info.rxdataF[0], ru_info.rxdataF[1]);
   start_meas(&ru->rx_fhaul);
-  ret = xran_fh_rx_read_slot(&ru_info, &f, &sl);
+  int ret = xran_fh_rx_read_slot(&ru_info, &f, &sl);
   stop_meas(&ru->rx_fhaul);
   LOG_D(HW, "Read %d.%d rxdataF %p,%p\n", f, sl, ru_info.rxdataF[0], ru_info.rxdataF[1]);
   if (ret != 0) {
     printf("ORAN: %d.%d ORAN_fh_if4p5_south_in ERROR in RX function \n", f, sl);
-  }
-
-  /* Secondly, process PRACH packets */
-  int f_prach, sl_prach;
-  ret = xran_fh_rx_prach_read_slot(ru->gNB_list[0], &ru_info, &f_prach, &sl_prach);
-  if (ret != 0) {
-    printf("ORAN: %d.%d ORAN_fh_if4p5_south_in ERROR in RX PRACH function \n", f_prach, sl_prach);
   }
 
   int slots_per_frame = 10 << (ru->openair0_cfg.nr_scs_for_raster);
@@ -306,7 +295,6 @@ __attribute__((__visibility__("default"))) int transport_init(openair0_device_t 
   // create message queues for ORAN sync
 
   initNotifiedFIFO(&oran_sync_fifo);
-  initNotifiedFIFO(&oran_sync_fifo_prach);
 
   eth->nCC = fh_config->nCC;
   eth->num_ports = fh_init.xran_ports;
